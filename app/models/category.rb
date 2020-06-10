83cc51250d2bb7e17f804e8880b8600f2bb8013b
MANOMOY BISWAS <manomoybiswas95@gmail.com>
2020-06-10 23:35:08 +0530

    Added Rspec for testing, added code for  testing Caregory

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,7 +1,8 @@
 class Category < ApplicationRecord
   before_validation { self.name = name.to_s.titleize.strip }
 
-  validates :name, presence: true, uniqueness: true, length: { maximum: 50 }
+  validates :name, presence: true, length: { in: 2..30 }
+  validates_uniqueness_of :name, case_sensitive: false
 
   has_many :tasks
   
---
+++ spec/mailers/previews/task_preview.rb
@@ -0,0 +1,3 @@
+class TaskPreview < ActionMailer::Preview
+
+end
