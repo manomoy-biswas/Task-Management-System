a71c9031804f2f6cd2492ddef902a87f583c5584
MANOMOY BISWAS <manomoybiswas95@gmail.com>
2020-06-20 02:41:33 +0530

    Solving download error and Ci error

--- app/models/task.rb
+++ app/models/task.rb
@@ -116,40 +116,6 @@ class Task < ApplicationRecord
     end  
   end
 
-  def self.download_document(task_id)
-    @task = self.find(task_id)
-    if @task.task_document.count == 1
-      data = open(@task.task_document.first.document.url)
-      send_data(data.read, type: data.content_type, filename: @task.task_document.first.document.file.filename)
-    else
-      folder_path = "#{Rails.root}/public/uploads/task_document/document/#{@task.id.to_s}/"
-      filename = @task.task_name + "_TD_" + DateTime.parse(@task.created_at.to_s).strftime("%d%m%Y") +".zip"
-      temp_file = Tempfile.new(filename)
-      FileUtils.remove_dir(folder_path) if Dir.exist?(folder_path)
-      Dir.mkdir(folder_path)
-      @task.task_document.each do |file|
-        open(folder_path + "#{file.document.file.filename}", 'wb') do |f|
-          f << open("#{file.document.url}").read
-        end
-      end
-
-      begin
-        Zip::OutputStream.open(temp_file) { |zos| }
-        Zip::File.open(temp_file.path, Zip::File::CREATE) do |zip|
-          @task.task_document.each do |file|
-            zip.add(file.document.file.filename, File.join(folder_path, file.document.file.filename))
-          end
-        end
-        read_data = File.read(temp_file.path)
-        send_data(read_data, type: "application/zip", filename: filename)
-      ensure
-        FileUtils.remove_dir(folder_path) if Dir.exist?(folder_path)
-        temp_file.close
-        temp_file.unlink
-      end
-    end
-  end
-
   def self.task_list_printing
     @tasks = self.notified_tasks
     respond_to do |format|
