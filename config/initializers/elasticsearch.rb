de0b2a3961e32c162ea9d9773edcc837cf467e55
Manomoy Biswas <manomoybiswas95@gmail.com>
2020-08-13 17:55:11 +0530

    elastic search initializer fix

--- config/initializers/elasticsearch.rb
+++ config/initializers/elasticsearch.rb
@@ -1,5 +1,5 @@
 if Rails.env.production?
   Elasticsearch::Model.client = Elasticsearch::Client.new url: ENV['BONSAI_URL']
-else
+elsif Rails.env.development?
   Elasticsearch::Model.client = Elasticsearch::Client.new url: "http://elasticsearch:9200"
 end
