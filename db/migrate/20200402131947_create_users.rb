8b370233209c136d393aa8ea9cbdcad663837c4d
Manomoy Biswas <manomoybiswas95@gmail.com>
2020-08-13 23:38:38 +0530

    migration updated

--- db/migrate/20200402131947_create_users.rb
+++ db/migrate/20200402131947_create_users.rb
@@ -1,10 +1,10 @@
 class CreateUsers < ActiveRecord::Migration[6.0]
   def change
     create_table :users do |t|
-      t.string     :email
+      t.string     :email,                 null: false, limit: 50
       t.boolean    :admin, default: false
       t.boolean    :hr,    default: false
-      t.string     :name,                  null: false
+      t.string     :name,                  null: false, limit: 30
       t.string     :phone
       t.date       :dob,                   null: false
       t.string     :password_digest,       null: true
@@ -13,7 +13,7 @@ class CreateUsers < ActiveRecord::Migration[6.0]
       t.datetime   :password_reset_sent_at
       t.timestamps
     end
-    add_index :users, :email, unique: true, length: 255
-    add_index :users, :phone, unique: true, length: 255
+    add_index :users, :email, unique: true, length: 191
+    add_index :users, :phone, unique: true, length: 191
   end
 end
